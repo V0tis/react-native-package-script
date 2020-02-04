04ed4f72b2ff223980da37efd8c50da6505649f3
utaha <48436555+JungDonggun@users.noreply.github.com>
2020-02-04 14:10:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,7 @@
     "device": "react-native run-ios --device '정동건의 iPhone Xs Max'",
     "re_install": "yarn && cd ios && pod install && yarn run iphone_se_start",
     "remove": "rm -rf node_modules && cd ios && rm -rf Pods && rm -rf build && rm -rf Podfile.lock && cd .. && yarn run re_install",
-    "iphone_se_start": "react-native run-ios --simulator='iPhone SE'"
+    "iphone_se_start": "react-native run-ios --simulator='iPhone SE'",
+    "make_app_icon": "react-native set-icon --path [path-to-image] --background [\"color\"]",
+    "make_splash": "react-native set-splash --path [path-to-image] --resize [contain|cover|center] --background [\"background-color\"]"
 }
