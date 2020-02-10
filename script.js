bcd2abed60c85918ba3df503bbcdd4f1d5d7c13e
utaha <48436555+JungDonggun@users.noreply.github.com>
2020-02-10 17:04:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,6 @@
     "remove": "rm -rf node_modules && cd ios && rm -rf Pods && rm -rf build && rm -rf Podfile.lock && cd .. && yarn run re_install",
     "iphone_se_start": "react-native run-ios --simulator='iPhone SE'",
     "make_app_icon": "react-native set-icon --path [path-to-image] --background [\"color\"]",
-    "make_splash": "react-native set-splash --path [path-to-image] --resize [contain|cover|center] --background [\"background-color\"]"
+    "make_splash": "react-native set-splash --path [path-to-image] --resize [contain|cover|center] --background [\"background-color\"]",
+    "project_rename": "react-native-rename <newName>"
 }
