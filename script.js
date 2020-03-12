c9a8e9420557647649be46c50e4545a103dce000
utaha <48436555+JungDonggun@users.noreply.github.com>
2020-03-12 20:15:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,7 @@
     "iphone_se_start": "react-native run-ios --simulator='iPhone SE'",
     "make_app_icon": "react-native set-icon --path [path-to-image] --background [\"color\"]",
     "make_splash": "react-native set-splash --path [path-to-image] --resize [contain|cover|center] --background [\"background-color\"]",
-    "project_rename": "react-native-rename <newName>"
+    "project_rename": "react-native-rename <newName>",
+        "android-debug-build": "react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/ && cd android && ./gradlew assembleDebug && cd ..",
+    "android-release-build": "react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/intermediates/res/merged/release/ && rm -rf android/app/src/main/res/drawable-* && rm -rf android/app/src/main/res/raw/* && cd android && ./gradlew assembleRelease && cd .."
 }
